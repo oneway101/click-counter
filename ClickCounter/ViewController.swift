7c98f4fd78673732a4d4d24a33cd58805a5c0322
oneway101 <oneway.first@gmail.com>
2016-08-01 17:16:18 -0400

    view controller count and label

--- ClickCounter/ViewController.swift
+++ ClickCounter/ViewController.swift
@@ -10,5 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var count = 0
+    var label:UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //label
+        var label = UILabel()
+        label.frame = CGRectMake(150,150,60,60)
+        label.text = "0"
+        
+        self.view.addSubview(label)
+        self.label = label
+        //button
+        var button = UIButton()
+        button.frame = CGRectMake(150, 250, 60, 60)
+        button.setTitle("Click", forState: .Normal)
+        button.setTitleColor(UIColor.blueColor(), forState: .Normal)
+        
+        self.view.addSubview(button)
+    }
+    func incrementCount(){
+        self.count++
+        self.label.text = "\(self.count)"
+    }
+
 }
 
